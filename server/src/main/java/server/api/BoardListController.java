cd1ee2aa555b936ce7550ee3a2e15d19e98e3b56
Matthias van Vliet <m.b.vanvliet@student.tudelft.nl>
2023-03-05 22:26:18 +0100

    Added basic REST-controller on top of the JpaRepositories

---
+++ server/src/main/java/server/api/BoardListController.java
@@ -0,0 +1,65 @@
+package server.api;
+
+import commons.Board;
+import commons.BoardList;
+import commons.Card;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.database.BoardListRepository;
+import server.database.BoardRepository;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/boardlists")
+public class BoardListController {
+
+    /**
+     * repo - the JpaRepository that is used for all items of type BoardList
+     */
+    private final BoardListRepository repo;
+
+    /**
+     * parentRepo - the JpaRepository that is used for all items of type Board. This repo is necessary
+     * for retrieving the value of the parentBoard field when constructing a new BoardList
+     */
+    private final BoardRepository parentRepo;
+
+    public BoardListController(BoardListRepository repo, BoardRepository parentRepo){
+        this.repo = repo;
+        this.parentRepo = parentRepo;
+    }
+
+    /**
+     * Gets a BoardList by id
+     * @param id - the id of the BoardList that has to be retrieved
+     * @return a ResponseEntity containing the BoardList that was requested by id, or a badrequest error if id is invalid
+     */
+    @GetMapping("/{id}")
+    ResponseEntity<BoardList> getById(@PathVariable("id") long id){
+        if(id < 0 || !repo.existsById(id)){
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(repo.findById(id).get());
+
+    }
+
+    /**
+     * Creates a new BoardList as a child of the board for which the id is passed
+     * @param boardId - the id of the board to which the BoardList should be added
+     * @param name - the name that will be given to the newly created BoardList
+     * @return a ResponseEntity containing the newly created BoardList or a badrequest error if id is invalid
+     */
+    @GetMapping("/new-boardlist/{boardId}/{name}")
+    ResponseEntity<BoardList> getNewList(@PathVariable("boardId") long boardId, @PathVariable("name") String name){
+        if(boardId < 0 || !parentRepo.existsById(boardId)){
+            return ResponseEntity.badRequest().build();
+        }
+        Board parent = parentRepo.getById(boardId);
+        BoardList listToBeAdded = new BoardList(name, new ArrayList<Card>(), parent);
+        BoardList addedList = repo.save(listToBeAdded);
+        return ResponseEntity.ok(addedList);
+    }
+
+
+}
