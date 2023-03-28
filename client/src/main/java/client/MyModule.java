ff556ab25ef25354d37c7dec725a107c031b2b5e
Marius Frija <m.frija@student.tudelft.nl>
2023-03-28 15:36:42 +0200

    Removed all files not related to the application

--- client/src/main/java/client/MyModule.java
+++ client/src/main/java/client/MyModule.java
@@ -24,9 +24,6 @@ public class MyModule implements Module {
     @Override
     public void configure(Binder binder) {
         binder.bind(MainCtrl.class).in(Scopes.SINGLETON);
-//        OBSOLETE CODE FOR QUOTE APPLICATION
-        binder.bind(AddQuoteCtrl.class).in(Scopes.SINGLETON);
-        binder.bind(QuoteOverviewCtrl.class).in(Scopes.SINGLETON);
         /*
          * Temporary for Single-board implementation
          */
