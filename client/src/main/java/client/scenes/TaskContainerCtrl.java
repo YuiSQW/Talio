a72ad4b5f5bb15734f8bbad5dc37e2adaf2c8cc7
Matei Hristodorescu <M.Hristodorescu@student.tudelft.nl>
2023-03-29 19:27:59 +0300

    test

--- client/src/main/java/client/scenes/TaskContainerCtrl.java
+++ client/src/main/java/client/scenes/TaskContainerCtrl.java
@@ -19,7 +19,6 @@ public class TaskContainerCtrl extends VBox{
 
     private final ServerUtils serverUtils;
 
-    private ListView<Card> listView;
 
     @Inject
     public TaskContainerCtrl(MainCtrl mainCtrl, ServerUtils serverUtils) {
@@ -31,19 +30,23 @@ public class TaskContainerCtrl extends VBox{
         this.addCardCtrl=addCardCtrl;
         this.task=new Task(addCardCtrl.getCard(),"Empty");
         //this.task=this.serverUtils.postNewTask(this.task,this.addCardCtrl.getCard());
+        TextField text= new TextField("empty");
+        text.setMinHeight(25.0);
+        text.setPrefHeight(25.0);
         Button button = new Button("x");
-        button.setMinHeight(100.0);
-        button.setMaxHeight(100.0);
-        button.setPrefHeight(100.0);
-        button.setPrefWidth(100.0);
+        button.setMinHeight(25.0);
+        button.setMaxHeight(25.0);
+        button.setPrefHeight(25.0);
+        button.setPrefWidth(25.0);
         button.setStyle("-fx-background-radius: 1em;");
 
-        HBox taskHbox=new HBox(button);
-        taskHbox.setMinHeight(100.0);
+        HBox taskHbox=new HBox(text,button);
+        taskHbox.setMinHeight(25.0);
         taskHbox.setMaxHeight(100.0);
-        taskHbox.setPrefHeight(200.0);
+        taskHbox.setPrefHeight(25.0);
         taskHbox.setPrefWidth(200.0);
         ///tilePane.setMargin(this, new Insets(10,0,0,0));
+
         getChildren().addAll(taskHbox);
     }
 }
