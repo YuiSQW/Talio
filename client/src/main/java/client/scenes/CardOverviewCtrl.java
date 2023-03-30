602793785003ee84985eb456f74543b65f91ab1d
Matei Hristodorescu <M.Hristodorescu@student.tudelft.nl>
2023-03-30 11:55:17 +0300

    removed redundant imports

--- client/src/main/java/client/scenes/CardOverviewCtrl.java
+++ client/src/main/java/client/scenes/CardOverviewCtrl.java
@@ -14,7 +14,6 @@ import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
 import java.util.Iterator;
 
 public class CardOverviewCtrl {
