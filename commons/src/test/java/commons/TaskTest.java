c667ac7df302a52f94113e281ee8026334ece396
Yulia Waarsenburg <Y.S.Q.Waarsenburg@student.tudelft.nl>
2023-04-04 17:22:52 +0200

    Tests for commons

--- commons/src/test/java/commons/TaskTest.java
+++ commons/src/test/java/commons/TaskTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 /**
  * Tests for the task class
@@ -39,6 +40,47 @@ public class TaskTest {
         Task task2=new Task(c,"Name");
         assertEquals(task,task2);
     }
+    
+    /**
+     * Test for the equals method not equal
+     */
+    @Test
+    void equalsTestNotEqual() {
+        Board t = new Board("", new ArrayList<>());
+        BoardList r  = new BoardList("", new ArrayList<>(), t);
+        Card c = new Card("cardTitle", "card description", r);
+        Task task=new Task(c,"Name");
+        Task task2=new Task(c,"Name");
+        task2.setId(1);
+        assertNotEquals(task,task2);
+    }
+    
+    /**
+     * Test for the equals method
+     */
+    @Test
+    void equalsTestNull() {
+        Board t = new Board("", new ArrayList<>());
+        BoardList r  = new BoardList("", new ArrayList<>(), t);
+        Card c = new Card("cardTitle", "card description", r);
+        Task task=new Task(c,"Name");
+        assertNotEquals(task,null);
+    }
+   
+    /**
+     * Test for the hashcode method
+     */
+    @Test
+    void hashCodeTest() {
+        Board t = new Board("", new ArrayList<>());
+        BoardList r  = new BoardList("", new ArrayList<>(), t);
+        Card c = new Card("cardTitle", "card description", r);
+        Task task1 = new Task(c, "Name");
+        Task task=new Task(c,"Name");
+        assertEquals(task.hashCode(),task1.hashCode());
+    }
+    
+    
     @Test
     void parentCardTest(){
         Board t = new Board("", new ArrayList<>());
