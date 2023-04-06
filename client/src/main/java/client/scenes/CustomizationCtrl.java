b50ddec6f26a81462b94813580f862a0b417a410
Zico Krassenburg <z.i.k.krassenburg@student.tudelft.nl>
2023-04-06 12:11:02 +0200

    Got board color changing working

--- client/src/main/java/client/scenes/CustomizationCtrl.java
+++ client/src/main/java/client/scenes/CustomizationCtrl.java
@@ -3,8 +3,13 @@ package client.scenes;
 import client.utils.ServerUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 import javax.inject.Inject;
 
 
@@ -13,11 +18,15 @@ public class CustomizationCtrl {
     private ServerUtils serverUtils;
     private Stage stage;
     private CustomizationCtrl customizationCtrl;
+    private BoardOverviewCtrl boardOverviewCtrl;
+    private ListContainerCtrl listContainerCtrl;
     @FXML
     private Pane toolBar;
     private double x,y;
     @FXML
-    private Button closeButton, minimizeButton;
+    private Button closeButton, minimizeButton, boardreset, listreset;
+    @FXML
+    private ColorPicker boardbackgroundcp, boardfontcp, listbackgroundcp, listfontcp;
     
     @Inject
     public CustomizationCtrl(MainCtrl mainCtrl, ServerUtils serverUtils){
@@ -25,8 +34,9 @@ public class CustomizationCtrl {
         this.serverUtils=serverUtils;
     }
 
-    public void init(Stage stage){
+    public void init(Stage stage, BoardOverviewCtrl boardOverviewCtrl){
         this.stage=stage;
+        this.boardOverviewCtrl=boardOverviewCtrl;
         
         toolBar.setOnMousePressed( mouseEvent -> {
             this.x= mouseEvent.getSceneX();
@@ -41,9 +51,41 @@ public class CustomizationCtrl {
     public void close(){
         this.stage.close();
     }
-    
+
     public void minimize(){
         this.stage.setIconified(true);
+    }
+
+    public void boardreset(){
+        //reseting the background and colorpicker
+        BackgroundFill backgroundFill = new BackgroundFill(Color.WHITE, null, null);
+        Background background = new Background(backgroundFill);
+        boardOverviewCtrl.gettilepane().setBackground(background);
+        boardbackgroundcp.setValue(Color.WHITE);
+
+        //reseting the font and colorpicker
+        boardOverviewCtrl.getboardTitle().setStyle(null);
+    }
+
+    public void boardchangebackground(){
+        BackgroundFill backgroundFill = new BackgroundFill(boardbackgroundcp.getValue(), null, null);
+        Background background = new Background(backgroundFill);
+        boardOverviewCtrl.gettilepane().setBackground(background);
+    }
+
+    public void boardchangefont(){
+        Color value = boardfontcp.getValue();
+        String style = String.format("-fx-text-fill: %s;", toHexString(value));
+        boardOverviewCtrl.getboardTitle().setStyle(style);
+    }
+
+    private static String toHexString(Color color) {
+        int r = ((int) Math.round(color.getRed()     * 255)) << 24;
+        int g = ((int) Math.round(color.getGreen()   * 255)) << 16;
+        int b = ((int) Math.round(color.getBlue()    * 255)) << 8;
+        int a = ((int) Math.round(color.getOpacity() * 255));
+    
+        return String.format("#%08X", (r + g + b + a));
     }
     
 }
